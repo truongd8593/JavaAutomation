4c4a79246aba3c768b4a47f1b007d72a46093c69
DANG Truong <dangtruong080593@gmail.com>
2020-10-25 11:47:57 +0700

    update tc

--- src/test/java/demo/TC001.java
+++ src/test/java/demo/TC001.java
@@ -3,6 +3,8 @@ package demo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,21 +14,29 @@ import common.functions.WebSupport;
 public class TC001 {
 
 	public static final Logger logger = LogManager.getLogger("DEMO");
+
+	private final String googleSearchInput = "//input[@name='q']";
+
+	private final String  tmaSearchResult = "//a[@href='https://www.tma.vn/']";
+
+	private final String googleHomePage = "https://www.google.com/";
+
+	private final String googleHomePageTitle = "Google";
 	
 	@Test
 	public void testGoogleSearch() throws Exception {
 		WebDriver driver = DriverFactory.createDriver();
 		WebSupport webSupport = new WebSupport(driver);
-		webSupport.navigateToUrl("https://www.google.com/")
+		webSupport.navigateToUrl(googleHomePage)
 				.maximizeBrowserWindow();
-		Assert.assertTrue(webSupport.verifyWebTitle("Google"));
+		Assert.assertTrue(webSupport.verifyWebTitle(googleHomePageTitle));
 		webSupport.captureScreenshot(driver, "Screenshots/TC001_ss1.png")
-				.sendKeysToElement("//*[@name='q']", "TMA Solutions")
+				.sendKeysToElement(googleSearchInput, "TMA Solutions")
 				.captureScreenshot(driver, "Screenshots/TC001_ss2.png")
-				.clickOnEnter("//*[@name='q']")
+				.clickOnEnter(googleSearchInput)
 				.waitSomeSeconds(1)
 				.captureScreenshot(driver, "Screenshots/TC001_ss3.png")
-				.clickOnElement("//a[@href='https://www.tma.vn/']")
+				.clickOnElement(tmaSearchResult)
 				.waitSomeSeconds(3)
 				.captureScreenshot(driver, "Screenshots/TC001_ss4.png");
 		driver.quit();
