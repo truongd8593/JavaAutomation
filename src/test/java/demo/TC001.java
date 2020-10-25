713dd4a38efd5e139ba7332f8ed5c88d95686f04
DANG Truong <dangtruong080593@gmail.com>
2020-10-25 08:22:16 +0700

    Remove unused libraries

--- src/test/java/demo/TC001.java
+++ src/test/java/demo/TC001.java
@@ -2,16 +2,10 @@ package demo;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import common.functions.DriverFactory;
-import common.functions.ReadExcel;
 import common.functions.WebSupport;
 
 public class TC001 {
